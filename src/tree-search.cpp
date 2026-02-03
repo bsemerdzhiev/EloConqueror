b7b54c3476a4282586edae2a7e9e9519cbd5c0bb
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-02-03 01:14:02 +0100

    replace recursion with linear traverse

--- src/tree-search.cpp
+++ src/tree-search.cpp
@@ -1,30 +1,58 @@
 #include "tree-search.hpp"
 #include "search.hpp"
 #include "undo_move.hpp"
-
-uint64_t rec_search(Board &board, int32_t depth,
-                    std::vector<Move> new_moves[]) {
-  if (depth == 0) {
-    return 1;
-  }
-  new_moves[depth].clear();
-  MoveExplorer::searchAllMoves(board, board.getPlayerTurn(), new_moves[depth]);
-
-  uint64_t cnt = 0;
-  for (const auto &next_move : new_moves[depth]) {
-    UndoMove undo_move;
-    board.makeMove(next_move.pos_from, next_move.pos_to, next_move.piece_type,
-                   board.getPlayerTurn(), next_move.move_type, undo_move);
-    cnt += rec_search(board, depth - 1, new_moves);
-    board.unmakeMove(undo_move);
-  }
-  return cnt;
-}
+#include <iostream>
+#include <stack>
 
 uint64_t TreeSearch::search(Board &board, int32_t depth) {
   std::vector<Move> new_moves[10];
+  int32_t visited[10] = {0};
   for (int32_t i = 0; i < 10; i++) {
     new_moves[i].reserve(256);
   }
-  return rec_search(board, depth, new_moves);
+
+  int64_t cnt = 0;
+  int32_t cur_depth = depth;
+  UndoMove undo_move;
+
+  std::array<UndoMove, 10> undo_moves;
+
+  MoveExplorer::searchAllMoves(board, board.getPlayerTurn(), new_moves[depth]);
+  while (true) {
+    if (cur_depth == 0) {
+      cnt += 1;
+      cur_depth++;
+
+      board.unmakeMove(undo_moves[cur_depth]);
+
+      continue;
+    } else if (visited[cur_depth] == new_moves[cur_depth].size()) {
+      cur_depth++;
+      if (cur_depth > depth) {
+        break;
+      }
+
+      board.unmakeMove(undo_moves[cur_depth]);
+
+      continue;
+    }
+
+    Move move_to_make = new_moves[cur_depth][visited[cur_depth]];
+    visited[cur_depth] += 1;
+
+    board.makeMove(move_to_make.pos_from, move_to_make.pos_to,
+                   move_to_make.piece_type, board.getPlayerTurn(),
+                   move_to_make.move_type, undo_move);
+
+    undo_moves[cur_depth] = undo_move;
+    if (cur_depth - 1 > 0) {
+      new_moves[cur_depth - 1].clear();
+      MoveExplorer::searchAllMoves(board, board.getPlayerTurn(),
+                                   new_moves[cur_depth - 1]);
+      visited[cur_depth - 1] = 0;
+    }
+    cur_depth--;
+  }
+
+  return cnt;
 }
